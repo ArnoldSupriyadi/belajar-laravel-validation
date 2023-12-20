2a081dd5814272ff00ec63b7d1b0e5055cfa2a2e
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2023-12-20 11:23:09 +0700

    baru sampai video 235. additional validation

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
@@ -83,6 +84,8 @@ class ValidatorTest extends TestCase
 
     public function testValidatorValidationMultipleRules()
     {
+        App::setLocale("id");
+
         $data = [
             "username" => "eko",
             "password" => "eko"
@@ -129,5 +132,36 @@ class ValidatorTest extends TestCase
             $message = $exception->validator->errors();
             Log::error($message->toJson(JSON_PRETTY_PRINT));
         }
+    }
+
+    public function testValidatorInLineMessage()
+    {
+
+        $data = [
+            "username" => "eko",
+            "password" => "eko"
+        ];
+
+        $rules = [
+            "username" => "required|email|max:100",
+            "password" => ["required","min:6", "max:20"]
+        ];
+
+        $messages = [
+            "required" => ":attribute harus diisi",
+            "email" => ":attribute harus berupa email",
+            "min" => ":attribute minimal :min karakter",
+            "max" => ":attribute maksimal :max karakter"
+        ];
+
+        $validator = Validator::make($data, $rules, $messages);
+        self::assertNotNull($validator);
+
+        self::assertFalse($validator->passes());
+        self::assertTrue($validator->fails());
+
+        $message = $validator->getMessageBag();
+        
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
     }
 }
